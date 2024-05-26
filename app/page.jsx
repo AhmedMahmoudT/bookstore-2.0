8885da71300f61803832ffa88cd18adc214b0f5c
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2024-05-26 17:29:37 +0100

    changed Link navigation to useRouter

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,20 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [loadingAfter, setLoadingAfter] = useState(false);
+
+  const handleRouting = (id) => {
+    setLoadingAfter(true)
+    const link = '/bookDetails/'+id
+    router.push(link)
+  }
 
   useEffect(() => {
     const q = query(collection(db, "books"));
@@ -31,13 +40,13 @@ export default function Home() {
           <div className="mt-20 grid grid-cols-3 gap-y-24 w-auto h-[750px] overflow-scroll no-scrollbar">
             {books.map((book,key)=>{
               return (
-                <Link href={`/bookDetails/${book.id}`} onClick={()=>setLoadingAfter(true)} key={key} className="flex flex-col items-center justify-center relative mx-20 hover:scale-110 transition-all">
+                <button onClick={()=>handleRouting(book.id)} key={key} className="flex flex-col items-center justify-center relative mx-20 hover:scale-110 transition-all cursor-pointer">
                   <Image src={book.coverImage} alt={book.title} width={2000} height={2000} style={{width: '250px', height: '400px'}}/>
                   <div className="text-xl w-[250px] absolute -bottom-12 h-10 flex justify-between">
                     <h1 className="my-auto w-3/4">{book.title}</h1>
                     <h1 className="my-auto"><sup>$</sup>{book.price}</h1>
                   </div>
-                </Link>
+                </button>
               )
             })}
           </div>
