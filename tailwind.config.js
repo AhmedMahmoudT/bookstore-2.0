803f56818807dad01ff30cf349697b84179f2b7b
AhmadMahmoudTarfaoui <114869552+AhmadMahmoudTarfaoui@users.noreply.github.com>
2024-05-26 23:36:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'intorange': {
+          50:  '#fff7ec',
+          100: '#ffedd3',
+          200: '#ffd8a5',
+          300: '#ffbb6d',
+          400: '#ff9232',
+          600: '#ff730a',
+          600: '#ff5900',
+          700: '#cc3e02',
+          800: '#a1310b',
+          900: '#822b0c',
+          950: '#461204',
+        },
+        // ...
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
